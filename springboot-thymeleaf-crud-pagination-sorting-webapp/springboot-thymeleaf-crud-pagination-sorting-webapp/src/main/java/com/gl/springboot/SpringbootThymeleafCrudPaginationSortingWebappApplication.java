f9cffe62af875073a2f1a8994f350e7fe4cc899c
chitranshi <109790176+chitranshi29@users.noreply.github.com>
2023-01-21 14:52:31 +0530

    Add files via upload

---
+++ springboot-thymeleaf-crud-pagination-sorting-webapp/springboot-thymeleaf-crud-pagination-sorting-webapp/src/main/java/com/gl/springboot/SpringbootThymeleafCrudPaginationSortingWebappApplication.java
@@ -0,0 +1,13 @@
+package com.gl.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafCrudPaginationSortingWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafCrudPaginationSortingWebappApplication.class, args);
+	}
+
+}
